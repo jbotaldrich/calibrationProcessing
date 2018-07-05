46c91edb323555b1630662e970cc4a520ff9ded6
Joshua Aldrich <jbot.aldrich@gmail.com>
2018-07-05 12:05:18 -0700

    Bug fix and a bunch of comments.

--- calibrationProcessing.m
+++ calibrationProcessing.m
@@ -7,11 +7,15 @@ kill;
 % retained to create provide concentrations values for plotting.
 %/Users/kderyckx/CloudStation/Computer\ A/KseniyaS/7032018/TestText
 %
-calibrationDir = '/Users/kderyckx/CloudStation/Computer A/KseniyaS/7032018/TestText';
+
+% Put your directory here.
+calibrationDir = '/Users/kderyckx/CloudStation/Computer A/KseniyaS/7032018/TestText'; %<<< CHANGE ME!!!
+
 % This may change later for the filename extension.
-test = strcat(calibrationDir, '/', '*.txt');
 files = dir(strcat(calibrationDir, '/', '*.txt'));
 delimiterIn = '\t';
+
+% Read in all the intensity files.
 initialIntensities = [];
 for c = 1:size(files)
     filename = strcat(files(c).folder, '/', files(c).name);
@@ -21,28 +25,28 @@ end
 
 % Generates the Wavenumbers
 % The variables set here can change between runs.
-calibrationFirst = getCalibrationDataPoint(1000, 19.7);
-calibrationSecond = getCalibrationDataPoint(1027, 19.63);
+calibrationFirst = getCalibrationDataPoint(1000, 19.7);  %<<< CHANGE ME!!!!
+calibrationSecond = getCalibrationDataPoint(1027, 19.63);  %<<< CHANGE ME!!!!
 [slope, intercept] = getCalibrationFxn(calibrationFirst, calibrationSecond);
 
 %This can change each run.
 waveNumbers = getWavenumbers(120, 0.005, 19.35, [slope, intercept]);
 
 % Does an initial plot of wavenumber vs intensity for each dataset.
-whaIsThis = files.name;
 plotAllLines(waveNumbers, files, initialIntensities);
 % Removes the background by subtracting minimum point in a specified range.
 intensitiesWithBackgroundRemoved = removeBackground([1010 1040], waveNumbers, initialIntensities);
 plotAllLines(waveNumbers, files, intensitiesWithBackgroundRemoved);
-l=legend();
 
+% Generate the peak ratios.
 peakRatio = getPeakRatio([955 975], [1070 1100], waveNumbers, intensitiesWithBackgroundRemoved);
 concentration = getConcentrationsFromFile(files);
 
 figure();
+% Scatter plot of the concentration vs peakRatio.
 scatter(concentration, peakRatio);
 
-
+% Parses the filename to pull out the concentration value.  Probably a more elegant way to do this.
 function concentrations = getConcentrationsFromFile(files)
 	concentrations = [];
 	for i = 1:size(files)
@@ -53,7 +57,8 @@ function concentrations = getConcentrationsFromFile(files)
 	end
 end
 
-
+% Generates the peak ratio as a ratio of 2 given wavelengths intensities.  Get the max value in 
+% in two wavelength ranges of interest and take the ratio.
 function peakRatios = getPeakRatio(firstPeakWaveNumberRange, secondPeakWaveNumberRange, wavenumbers, intensityArray)
 	[~, wavenumberTargetOneIndexStart] = min(abs(wavenumbers - firstPeakWaveNumberRange(1)));
 	[~, wavenumberTargetOneIndexEnd] = min(abs(wavenumbers - firstPeakWaveNumberRange(2)));
@@ -67,27 +72,36 @@ function peakRatios = getPeakRatio(firstPeakWaveNumberRange, secondPeakWaveNumbe
 	end
 end
 
+% This generates an excel spreadsheet of the intensities based on wavelength.
 function excelSpreadSheet = saveSpreadsheet(filename, headers, wavenumbers, intensityArray)
 	dataCells = num2cell(intensityArray);
 	outputMatrix = [{'wavenumber'} headers; wavenumbers dataCells];
 	xlswrite(filename, outputMatrix);
 end
 
+% Creates a data point 1x2 array provided a peak and stage distance.  (maybe change parameter
+% name from benzoPeak to just peak)
 function calibrationDataPoint = getCalibrationDataPoint(benzoPeak, stageDistance)
 	calibrationDataPoint = [benzoPeak stageDistance];
 end
 
+% Generates a calibration funciton based on start and end calibration data points.
 function [slope, intercept] = getCalibrationFxn(calibrationStart, calibrationEnd) 
 	slope = (calibrationStart(1) - calibrationEnd(1))./(calibrationStart(2) - calibrationEnd(2));
 	intercept = calibrationStart(1) - slope*calibrationStart(2);
 end
 
+%Converts stage information into wavenumbers.
 function wavenumbers = getWavenumbers(numberOfSteps, stepSize, startStage, calibrationCurve)
 	endStage = startStage + stepSize * numberOfSteps;
 	stageRange = linspace(startStage, endStage, numberOfSteps);
 	wavenumbers = fliplr(calibrationCurve(1) * stageRange + calibrationCurve(2));
 end
 
+% Subtract background expects 
+% removalRange, 1x2 array of start and end of removal range
+% wavenumbers, an array of 1-d array of wavenumbers (wavelengths)
+% intensityArray, array of intensity where each experiment is a column.
 function backgroundSubtracted = removeBackground(removalRange, wavenumbers, intensityArray)
 	[~, wavenumberIndexStart] = min(abs(wavenumbers - removalRange(1)));
 	[~, wavenumberIndexEnd] = min(abs(wavenumbers - removalRange(2)));
@@ -99,14 +113,14 @@ function backgroundSubtracted = removeBackground(removalRange, wavenumbers, inte
 	end
 end
 
+% Plots all the experiments in one figure.
 function someFigure = plotAllLines(wavenumbers, dataLabel, intensityArray)
 	someFigure = figure;
 	hold on;
     labels = [];
 	for i = 1:size(intensityArray, 2)
 		plot(wavenumbers, intensityArray(:,i), 'Linewidth', 2.0);
-        %labels = [labels, dataLabel(i).name];
-        %l=legend(files(1,i))
+        labels = [labels, dataLabel(i).name];
 	end
 	xlim([906 1135]);
 	xlabel('Raman Shift');
